2b61e847799ac2eb62e6f2feb038e845b43a9200
entropyfeng <1092758202@qq.com>
2021-04-10 13:57:33 +0800

    Update DetailDormitory.tsx

--- src/pages/apartment/admin/dormitory/components/DetailDormitory.tsx
+++ src/pages/apartment/admin/dormitory/components/DetailDormitory.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { DormitoryItem } from '@/pages/apartment/admin/dormitory/data';
-import { Card, Modal } from 'antd';
+import { Descriptions, Modal } from 'antd';
 import { useRequest } from '@@/plugin-request/request';
 import { acquireDetailDormitoryUsingGET } from '@/services/swagger/dormitoryController';
 
@@ -22,15 +22,31 @@ const DetailDormitory: React.FC<DetailDormitoryProps> = (props) => {
   if (error) {
     return <div>{error.message}</div>;
   }
+
   console.log(data);
+   // @ts-ignore
+  const dor= data.dormitory;
   return (  <Modal
     destroyOnClose
-    title="详情"
     visible={modalVisible}
     onCancel={() => onCancel()}
   >
-    <Card bordered={false}/>
 
+    <Descriptions title="公寓信息" bordered>
+      <Descriptions.Item label="ID">{dor.dormitoryId}</Descriptions.Item>
+      <Descriptions.Item label="公寓名称">{dor.dormitoryName}</Descriptions.Item>
+      <Descriptions.Item label="楼栋名称">{dor.buildingName}</Descriptions.Item>
+      <Descriptions.Item label="校区名">{dor.campusName}</Descriptions.Item>
+      <Descriptions.Item label="校区组名">{dor.campusGroupName}</Descriptions.Item>
+      <Descriptions.Item label="当前容量">{dor.currentCapacity}</Descriptions.Item>
+      <Descriptions.Item label="总容量">{dor.totalCapacity}</Descriptions.Item>
+      <Descriptions.Item label="楼层">{dor.floor}</Descriptions.Item>
+      <Descriptions.Item label="性别">{dor.inGender==="MAN"?"男":"女"}</Descriptions.Item>
+      <Descriptions.Item label="电梯">{dor.hasElevator?"有电梯":"无电梯"}</Descriptions.Item>
+      <Descriptions.Item label="朝向">{dor.dormitoryDirection}</Descriptions.Item>
+      <Descriptions.Item label="上次修改时间">{dor.updateTime}</Descriptions.Item>
+      <Descriptions.Item label="创建时间">{dor.createTime}</Descriptions.Item>
+    </Descriptions>
   </Modal>);
 }
 export default DetailDormitory;
